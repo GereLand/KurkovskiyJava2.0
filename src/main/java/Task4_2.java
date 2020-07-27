ef3ab6d1fa8ef1acd92cdd1b271eebd1cd297600
KurkovskiyDP( <Daniil.Kurkovskiy@appline.ru>
2020-07-27 21:24:38 +0300

    Завершил выполнение задания 4-2

--- src/main/java/Task4_2.java
+++ src/main/java/Task4_2.java
@@ -1,15 +1,32 @@
+
 public class Task4_2 {
     /**
      * Задание №4
-     * <p>
-     * 1. Массив размерностью 20, заполняется случайными целыми числами от -10 до 10.
-     * Найти максимальный отрицательный и минимальный положительный элементы массива. Поменять их местами.
      * <p>
      * 2. Формируется новогодний подарок. Он может включать в себя разные сладости (Candy, Jellybean, etc.)
      * У каждой сладости есть название, вес, цена и свой уникальный параметр. Необходимо собрать подарок из сладостей.
      * Найти общий вес подарка, общую стоимость подарка и вывести на консоль информацию о всех сладостях в подарке.
      */
+    /*
+    Ячейки массивов:
+    1. Название (Name);
+    2. Вес (Weight);
+    3. Цена (Price);
+    4. Уникальный параметр (Unique parameter).
+     */
     public static void main(String[] args) {
+        String[] Candys = {"Конфеты", "0.5", "1000", "Конфетное ассорти"};
+        String[] Jellybeans = {"Мармеладки", "0.3", "500", "Мармеладные мишки с колой"};
+        String[] Cookies = {"Печеньки", "1", "700", "Печеньки из коробки для хранения ниток"};
 
+        double weight = Double.parseDouble(Candys[1]) + Double.parseDouble(Jellybeans[1]) + Double.parseDouble(Cookies[1]);
+        int price = Integer.parseInt(Candys[2]) + Integer.parseInt(Jellybeans[2]) + Integer.parseInt(Cookies[2]);
+
+        System.out.println("Состав новогоднего подарка:");
+        System.out.println(Candys[0] + " " + Candys[1] + "кг за " + Candys[2] + " руб. (" + Candys[3] + ")");
+        System.out.println(Jellybeans[0] + " " + Jellybeans[1] + "кг за " + Jellybeans[2] + " руб. (" + Jellybeans[3] + ")");
+        System.out.println(Cookies[0] + " " + Cookies[1] + "кг за " + Cookies[2] + " руб. (" + Cookies[3] + ")");
+        System.out.println("Общий вес посылки = " + weight + " кг.");
+        System.out.println("Общая стоимость посылки = " + price + " руб.");
     }
 }
