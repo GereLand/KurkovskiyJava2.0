2c40b8a5a669223d13815ce35bb1a2bc1ab2e36f
KurkovskiyDP( <Daniil.Kurkovskiy@appline.ru>
2020-08-18 20:23:44 +0300

    Решение задания №5

---
+++ src/main/java/Calculation.java
@@ -0,0 +1,19 @@
+public class Calculation {
+
+    static double addition(double operand1, double operand2) {
+        return operand1 + operand2;
+    }
+
+    static double subtraction(double operand1, double operand2) {
+        return operand1 - operand2;
+    }
+
+    static double multiplication(double operand1, double operand2) {
+        return operand1 * operand2;
+    }
+
+    static double division(double operand1, double operand2) {
+        return operand1 / operand2;
+    }
+
+}
--- src/main/java/Task5.java
+++ src/main/java/Task5.java
@@ -14,34 +14,19 @@ public class Task5 {
         String sign = scan.next();
         System.out.print("Введите значение переменной В: ");
         double operand2 = scan.nextDouble();
+
         if ("+".equals(sign)) {
-            System.out.printf("Результат вычисления = " + ("%-10.2f%n%n"), addition(operand1, operand2));
+            System.out.printf("Результат вычисления = " + ("%-10.2f%n%n"), Calculation.addition(operand1, operand2));
         } else if ("-".equals(sign)) {
-            System.out.printf("Результат вычисления = " + ("%-10.2f%n%n"), subtraction(operand1, operand2));
+            System.out.printf("Результат вычисления = " + ("%-10.2f%n%n"), Calculation.subtraction(operand1, operand2));
         } else if ("*".equals(sign)) {
-            System.out.printf("Результат вычисления = " + ("%-10.2f%n%n"), multiplication(operand1, operand2));
+            System.out.printf("Результат вычисления = " + ("%-10.2f%n%n"), Calculation.multiplication(operand1, operand2));
         } else if ("/".equals(sign)) {
             if (operand2 != 0)
-                System.out.printf("Результат вычисления = " + ("%-10.2f%n%n"), division(operand1, operand2));
+                System.out.printf("Результат вычисления = " + ("%-10.2f%n%n"), Calculation.division(operand1, operand2));
             else System.out.println("Невозможно произвести операцию деления на ноль!");
         } else {
             System.out.print("Введено некорректное значение!");
         }
-    }
-
-    public static double addition(double operand1, double operand2) {
-        return operand1 + operand2;
-    }
-
-    public static double subtraction(double operand1, double operand2) {
-        return operand1 - operand2;
-    }
-
-    public static double multiplication(double operand1, double operand2) {
-        return operand1 * operand2;
-    }
-
-    public static double division(double operand1, double operand2) {
-        return operand1 / operand2;
     }
 }
