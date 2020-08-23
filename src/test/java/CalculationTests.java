d80127ca21a06a0098587df2e993de071ccd05e9
KurkovskiyDP( <Daniil.Kurkovskiy@appline.ru>
2020-08-23 23:15:02 +0300

    Добавил в настройки возможность использования unit тесты, начал создавать основу под тестовое покрытие калькулятора

---
+++ src/test/java/CalculationTests.java
@@ -0,0 +1,31 @@
+import Homework5.Calculation;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculationTests {
+
+    @Test
+    public void testAddition () {
+        Calculation calculation = new Calculation();
+//        Assert.assertTrue();
+    }
+
+    @Test
+    public void testSubtraction () {
+        Calculation calculation = new Calculation();
+
+    }
+
+    @Test
+    public void testMultiplication () {
+        Calculation calculation = new Calculation();
+
+    }
+
+    @Test
+    public void testDivision () {
+        Calculation calculation = new Calculation();
+
+    }
+
+}
