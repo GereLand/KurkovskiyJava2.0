8985986be8600479acd4ef89918d7d6a10a525a3
GereLand <Dan_9494@mail.ru>
2020-08-27 23:00:28 +0300

    Перенёс калькулятор, для того чтобы отдельно покрыть его исключениями

---
+++ src/main/java/Homework7/Calculation2.java
@@ -0,0 +1,20 @@
+package Homework7;
+
+public class Calculation2 {
+
+    static double addition(double operand1, double operand2) {
+        return operand1 + operand2;
+    }
+
+    static double subtraction(double operand1, double operand2) {
+        return operand1 - operand2;
+    }
+
+    static double multiplication(double operand1, double operand2) {
+        return operand1 * operand2;
+    }
+
+    static double division(double operand1, double operand2) {
+        return operand1 / operand2;
+    }
+}
--- src/main/java/Homework7/Task7.java
+++ src/main/java/Homework7/Task7.java
@@ -1,5 +1,7 @@
 package Homework7;
 
+import java.util.Scanner;
+
 public class Task7 {
     /**
      * Задание №7
@@ -9,7 +11,29 @@ public class Task7 {
      */
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите значение переменной А: ");
+        double operand1 = scan.nextDouble();
+        System.out.print("Введите знак арифметической операции: ");
+        String sign = scan.next();
+        System.out.print("Введите значение переменной В: ");
+        double operand2 = scan.nextDouble();
 
+        Calculation2 calculation = new Calculation2();
+
+        if ("+".equals(sign)) {
+            System.out.printf("Результат вычисления = " + ("%-10.2f%n%n"), calculation.addition(operand1, operand2));
+        } else if ("-".equals(sign)) {
+            System.out.printf("Результат вычисления = " + ("%-10.2f%n%n"), calculation.subtraction(operand1, operand2));
+        } else if ("*".equals(sign)) {
+            System.out.printf("Результат вычисления = " + ("%-10.2f%n%n"), calculation.multiplication(operand1, operand2));
+        } else if ("/".equals(sign)) {
+            if (operand2 != 0)
+                System.out.printf("Результат вычисления = " + ("%-10.2f%n%n"), calculation.division(operand1, operand2));
+            else System.out.println("Невозможно произвести операцию деления на ноль!");
+        } else {
+            System.out.print("Введено некорректное значение!");
+        }
     }
 }
 
